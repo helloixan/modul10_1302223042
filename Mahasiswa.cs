956fd9f273f9de58ba901a0443b0ac1f3099e243
helloixan <iksanrisandy@gmail.com>
2024-05-02 10:13:33 +0700

    menambahkan API Mahasiswa

---
+++ Mahasiswa.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace modul10_1302223042
+{
+    public class Mahasiswa
+    {
+        public string name { get; set; }
+        public string nim { get; set; }
+        public List<string> course { get; set; }
+        public int year { get; set; }
+
+        public Mahasiswa(string name, string nim, List<string> course, int year)
+        {
+            this.name = name;
+            this.nim = nim;
+            this.course = course;
+            this.year = year;
+        }
+
+    }
+}
